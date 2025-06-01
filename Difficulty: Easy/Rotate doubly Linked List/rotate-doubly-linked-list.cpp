de8a33ca8e413eab157d973a2b736da1a9150ed2
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-01 15:18:16 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Rotate doubly Linked List/rotate-doubly-linked-list.cpp
@@ -0,0 +1,53 @@
+/*
+class Node
+{
+public:
+    int data;
+    Node *prev;
+    Node *next;
+
+    Node(int x)
+    {
+        data = x;
+        prev = nullptr;
+        next = nullptr;
+    }
+};
+*/
+
+class Solution {
+  public:
+    Node *rotateDLL(Node *head, int p) {
+
+        // code here..
+        Node* tail = head;
+        while(tail->next)tail = tail->next;
+        tail->next = head;
+        head->prev = tail;
+        for(int count = 1 ; count <= p; count++){
+            head = head->next;
+            tail =tail->next;
+        }
+        tail->next = NULL;
+        head ->prev = NULL;
+        return head;
+        
+    }
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
