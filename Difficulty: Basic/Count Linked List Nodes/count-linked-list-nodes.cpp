d15a87ab1e170aa4aba52078807a263190a9d827
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-02 13:43:38 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Basic/Count Linked List Nodes/count-linked-list-nodes.cpp
@@ -0,0 +1,24 @@
+/* Link list node */
+/*
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int x) {  data = x;  next = NULL; }
+}; */
+
+class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    int getCount(struct Node* head) {
+
+        // Code here
+        int count = 0;
+        Node* curr = head;
+        while(curr){
+            curr = curr->next;
+            count++;
+        }
+        return count;
+    }
+};
