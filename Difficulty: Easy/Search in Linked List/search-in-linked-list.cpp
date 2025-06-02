028dbe93811ae9680504c58db4c71db03d9d6fd5
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-02 13:45:58 +0530

    Updated Solution - GfG to GitHub

---
+++ Difficulty: Easy/Search in Linked List/search-in-linked-list.cpp
@@ -0,0 +1,22 @@
+/* Link list node */
+/*
+struct Node
+{
+    int data;
+    Node* next;
+    Node(int x) {  data = x;  next = NULL; }
+}; */
+
+class Solution {
+  public:
+    // Function to count nodes of a linked list.
+    bool searchKey(int n, Node* head, int key) {
+        // Code here
+        Node* curr = head;
+        while(curr){
+            if(curr->data == key)return true;
+            curr = curr->next;
+        }
+        return false;
+    }
+};
