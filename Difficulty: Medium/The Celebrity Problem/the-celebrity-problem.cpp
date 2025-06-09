7e6444e0b1088ddf03beb02bc3aeb9493caee56b
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-09 08:44:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
+++ Difficulty: Medium/The Celebrity Problem/the-celebrity-problem.cpp
@@ -20,16 +20,14 @@ class Solution {
         int ans = s.top();
         //check row (all 0)
         for(int i = 0 ; i < n ; i++){
-            if(i != ans && mat[ans][i] == 1)return -1;
+            if( i != ans && mat[ans][i] == 1)return -1;
             
         }
         
         // /check col (all 1)
         for(int i = 0 ; i < n ; i++){
             if( i != ans && mat[i][ans] == 0)return -1;
-            
         }
-        
         return ans;
     }
 };
