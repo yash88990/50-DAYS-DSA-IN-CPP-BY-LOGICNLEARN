260758bf3c9f40708198875bf8ba09e80c2cf9d8
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-14 10:42:20 +0530

    Time: 19 ms (5.9%), Space: 19.7 MB (5.54%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.cpp
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -1,19 +1,77 @@
+
+class TrieNode{
+    public:
+    char data;
+    int childcount;
+    TrieNode* children[26];
+    bool isTerminal;
+    //constructor
+    TrieNode(char ch){
+        data = ch;
+        for(int i = 0 ; i < 26 ;i++){
+            children[i] = NULL;
+        }
+        childcount = 0;
+        isTerminal = false;
+    }
+};
+
+class Trie{
+    TrieNode* root  ;
+    //constructor
+    public:
+    Trie(){
+        root = new TrieNode('\0');
+    }
+    //insertion 
+    void insertWord(string word){
+        insertUtil(root , word);
+    }
+    void insertUtil(TrieNode* root , string word){
+        //base case
+        if(word.size() == 0){
+            root->isTerminal = true;
+            return;
+        }
+        int index = word[0] - 'a';
+        TrieNode* child;
+        //present
+        if(root->children[index] != NULL){
+            child = root->children[index];
+        }else{
+            //absent hai 
+            child = new TrieNode(word[0]);
+            root->childcount++;
+            root->children[index] = child;
+        }
+        //baki ka recc dekh lega 
+        insertUtil(child , word.substr(1));
+    }
+    void lcp(string str , string &ans){
+        for(int i = 0 ; i < str.size() ; i++){
+            char ch = str[i];
+            if(root->childcount == 1){
+                ans.push_back(ch);
+                int index = ch - 'a';
+                root = root->children[index];
+            }else break;
+            if(root->isTerminal)break;
+        }
+        
+    }
+};
 class Solution {
 public:
-    string longestCommonPrefix(vector<string>& arr) {
-        string ans = "";
-        for(int i = 0 ; i < arr[0].size() ; i++){
-            char ch = arr[0][i];
-            bool match = true;
-            for(int j = 1 ; j  < arr.size() ; j++){
-                if((arr[j].size() < i) ||( ch != arr[j][i])){
-                    match = false;
-                    break;
-                }
-            }
-            if(match == false) break;
-            else ans.push_back(ch);
+    string longestCommonPrefix(vector<string>& strs) {
+        Trie* t = new Trie();
+        for(int i = 0 ; i < strs.size() ; i++){
+            t->insertWord(strs[i]);
+             if(strs.size() == 0 || strs[i] == "")return "";
         }
+       
+        string first = strs[0];
+        string ans = "";
+        t->lcp(first , ans);
         return ans;
     }
 };
