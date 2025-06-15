83a49bb1dee8914293eaeaa33abbae5e32c432d0
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-15 08:55:08 +0530

    Time: 0 ms (100%), Space: 15.1 MB (25.02%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -26,8 +26,8 @@ public:
         //step 2 :- compute the position from starting 
         int pos = count - n;
         temp = head;
-        int i = 0;
-        while( i < pos - 1){
+        int i = 1;
+        while( i < pos ){
             temp = temp->next;
             i++;
         }
