271b849bac67a878f181d224eed60009863b204b
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-15 09:10:00 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Move Last Element to Front of a Linked List/move-last-element-to-front-of-a-linked-list.cpp
+++ Difficulty: Easy/Move Last Element to Front of a Linked List/move-last-element-to-front-of-a-linked-list.cpp
@@ -4,12 +4,15 @@ class Solution {
   public:
     Node *moveToFront(Node *head) {
         // code here
+        if(!head || !head->next)return head;
         Node* tail = head;
-        while(tail->next->next)tail = tail->next;
+        while(tail->next->next){
+            tail = tail->next;
+        }
         tail->next->next = head;
-        head = tail->next;
+         head = tail->next;
         tail->next = NULL;
+       
         return head;
-        
     }
 };
