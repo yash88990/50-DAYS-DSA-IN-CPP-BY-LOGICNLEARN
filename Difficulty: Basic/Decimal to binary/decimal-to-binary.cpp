da9b901c2b57c5732ba83b8122a1b4d29b5d78b0
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-20 07:36:28 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Decimal to binary/decimal-to-binary.cpp
+++ Difficulty: Basic/Decimal to binary/decimal-to-binary.cpp
@@ -2,10 +2,10 @@ class Solution {
   public:
     string decToBinary(int n) {
         // code here
-        if(n == 0)return "0";
+        if(n == 0 )return "0";
         string ans = "";
-        while(n > 0){
-            ans = to_string(n % 2 ) + ans;
+        while(n> 0){
+            ans = to_string(n % 2) + ans;
             n /= 2;
         }
         return ans;
