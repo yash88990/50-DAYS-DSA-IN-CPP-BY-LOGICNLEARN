34970c1850e0e25d84e474893043dc8127138729
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-23 18:09:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Easy/Leaves at Same Level or Not/leaves-at-same-level-or-not.cpp
@@ -0,0 +1,45 @@
+/* The structure of the binary tree is as follows
+struct Node
+{
+    int data;
+    Node* left;
+    Node* right;
+};
+*/
+
+
+class Solution {
+public:
+    bool check(Node *root) {
+        if (!root) {
+            return true;
+        }
+
+        queue<pair<Node*, int>> q;
+        q.push({root, 0});
+
+        int firstLeafLevel = -1;
+
+        while (!q.empty()) {
+            Node* curr_node = q.front().first;
+            int level = q.front().second;
+            q.pop();
+
+            if (!curr_node->left && !curr_node->right) { // It's a leaf node
+                if (firstLeafLevel == -1) { // This is the first leaf we've encountered
+                    firstLeafLevel = level;
+                } else if (level != firstLeafLevel) { // Not all leaves are at the same level
+                    return false;
+                }
+            } else { // Not a leaf node, explore children
+                if (curr_node->left) {
+                    q.push({curr_node->left, level + 1});
+                }
+                if (curr_node->right) {
+                    q.push({curr_node->right, level + 1});
+                }
+            }
+        }
+        return true;
+    }
+};
