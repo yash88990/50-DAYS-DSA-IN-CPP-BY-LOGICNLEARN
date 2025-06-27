b792c7f5c6d7f77f019803dfc2dd91dfc2190142
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-27 06:44:38 +0530

    Time: 0 ms (100%), Space: 18.9 MB (95.48%) - LeetHub

--- 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
+++ 0104-maximum-depth-of-binary-tree/0104-maximum-depth-of-binary-tree.cpp
@@ -15,7 +15,6 @@ public:
         if(!root)return 0;
         int left = maxDepth(root->left);
         int right = maxDepth(root->right);
-        int ans = max(left , right) + 1 ;
-        return ans;
+        return max(left , right) + 1 ;
     }
 };
