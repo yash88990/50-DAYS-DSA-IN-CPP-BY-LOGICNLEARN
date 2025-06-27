1dac75bfaff0f14363e625df9c042d3dc6e6bf94
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-27 12:54:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Split a Linked List into two halves/split-a-linked-list-into-two-halves.cpp
+++ Difficulty: Easy/Split a Linked List into two halves/split-a-linked-list-into-two-halves.cpp
@@ -14,36 +14,44 @@ struct Node
 //  function which splits the circular linked list.  head is pointer
 // to head Node of given lined list.  head1_ref1 and *head_ref2
 // are pointers to head pointers of resultant two halves.
+
 class Solution {
   public:
-    pair<Node *, Node *> splitList(Node *head) {
-        if (!head || head->next == head)
-            return {head, nullptr};
-
-        Node* slow = head;
-        Node* fast = head;
-
-        // Move slow and fast to find midpoint
-        while (fast->next != head && fast->next->next != head) {
+    pair<Node *, Node *> splitList(struct Node *head) {
+        // code here
+        if(!head || head->next == head)return {head , NULL};
+        Node* slow = head  , *fast = head;
+        //find middle
+        while(fast->next != head && fast->next->next != head){
             slow = slow->next;
             fast = fast->next->next;
         }
-
-        Node* firstHalf = head;
-        Node* secondHalf = slow->next;
-
-        // Make first half circular
-        slow->next = firstHalf;
-
-        // Make second half circular
-        Node* temp = secondHalf;
-        while (temp->next != head) {
+        //make two starting point
+        Node* firsthalf = head;
+        Node* secondhalf = slow->next;
+        //make fisrt haslf circular
+        slow->next = firsthalf;
+        //make second half as circular
+        Node* temp = secondhalf;
+        while(temp->next != head){
             temp = temp->next;
         }
-        temp->next = secondHalf;
-
-        return {firstHalf, secondHalf};
+        temp->next = secondhalf;
+        
+        
+        return {firsthalf , secondhalf};
+        
+        
+        
+        
     }
 };
+
+
+
+
+
+
+
 
 
