4f12d87acd584e3cca0e26d2cace9a24114ecb66
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-27 07:07:56 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sum Tree/sum-tree.cpp
+++ Difficulty: Medium/Sum Tree/sum-tree.cpp
@@ -11,10 +11,10 @@ private:
     pair<bool, long long> checkAndSum(Node* root) {
         if (!root) return {true, 0LL};
         if (!root->left && !root->right ) return {true, root->data};
-        pair<bool, long long> left_result = checkAndSum(root->left);
-        pair<bool, long long> right_result = checkAndSum(root->right);
-        if (!left_result.first || !right_result.first)return {false, 0LL};
-        long long children_subtrees_sum = left_result.second + right_result.second;
+        pair<bool, long long> leftans = checkAndSum(root->left);
+        pair<bool, long long> rightans = checkAndSum(root->right);
+        if (!leftans.first || !rightans.first)return {false, 0LL};
+        long long children_subtrees_sum = leftans.second + rightans.second;
         if (root->data == children_subtrees_sum)
             return {true, root->data + children_subtrees_sum};
         else return {false, 0LL};
