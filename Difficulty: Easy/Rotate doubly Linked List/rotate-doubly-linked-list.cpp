ae52b95bff7744722abb505d4229324521252c16
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-27 11:07:39 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Rotate doubly Linked List/rotate-doubly-linked-list.cpp
+++ Difficulty: Easy/Rotate doubly Linked List/rotate-doubly-linked-list.cpp
@@ -20,25 +20,27 @@ class Solution {
     Node *rotateDLL(Node *head, int p) {
 
         // code here..
+        if(!head)return head;
         Node* tail = head;
         while(tail->next)tail = tail->next;
+        //connect them
         tail->next = head;
         head->prev = tail;
-        for(int count = 1 ; count <= p; count++){
+        //move head and tail pointer by p times
+        for(int i =0 ;i < p ; i++){
             head = head->next;
-            tail =tail->next;
+            tail= tail->next;
         }
+        //break the circula loop
         tail->next = NULL;
-        head ->prev = NULL;
+        head->prev = NULL;
+        
         return head;
+        
+        
         
     }
 };
-
-
-
-
-
 
 
 
