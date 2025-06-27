e84b291e6457229e29deb48c976a17d6beadd326
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-27 07:45:23 +0530

    Time: 3 ms (41.58%), Space: 15.8 MB (72.04%) - LeetHub

--- 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
+++ 1029-vertical-order-traversal-of-a-binary-tree/1029-vertical-order-traversal-of-a-binary-tree.cpp
@@ -13,30 +13,34 @@ class Solution {
 public:
     vector<vector<int>> verticalTraversal(TreeNode* root) {
         vector<vector<int>>ans;
-        if(root == NULL)return ans;
-        map<int , map<int , vector<int>>> nodes;
-        queue<pair<TreeNode*, pair<int ,int>>> q;
+        if(!root)return ans;
+        // hd , level , list od nodes
+        map<int,map<int,vector<int>>> nodes;
+        // node hd level
+        queue<pair<TreeNode* , pair<int,int>>> q;
         q.push(make_pair(root , make_pair(0,0)));
         while(!q.empty()){
-            pair<TreeNode* , pair<int,int>> temp = q.front();
+            pair<TreeNode*, pair<int,int>> temp = q.front();
             q.pop();
             TreeNode* front = temp.first;
             int hd = temp.second.first;
-            int lvl = temp.second.second;
-            nodes[hd][lvl].push_back(front->val);
-            if(front->left)q.push(make_pair(front->left ,make_pair( hd - 1, lvl + 1)));
-            if(front->right)q.push(make_pair(front->right ,make_pair( hd + 1, lvl + 1 )));
-
+            int level = temp.second.second;
+            nodes[hd][level].push_back(front->val);
+            //left ja reh :- hd - 1
+            if(front->left)q.push(make_pair(front->left , make_pair(hd - 1 , level + 1)));
+            //right jaye :- hd + 1
+            if(front->right)q.push(make_pair(front->right , make_pair(hd + 1 ,level+1)));
         }
-        for (auto i : nodes) {
-            vector<int> col;
-            for (auto j : i.second) {
-                vector<int> temp = j.second;
-                sort(temp.begin(), temp.end());
-                col.insert(col.end(), temp.begin(), temp.end());
+        for(auto i : nodes){
+            vector<int>col;
+            for(auto j : i.second){
+                vector<int>temp = j.second;
+                sort(temp.begin() , temp.end());
+                col.insert(col.end() , temp.begin() , temp.end());
             }
             ans.push_back(col);
         }
         return ans;
+
     }
 };
