ae236288aabde690b9e83c4870e91e15eeefe4f4
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-28 13:21:43 +0530

    Time: 0 ms (100%), Space: 17.7 MB (14.31%) - LeetHub

--- 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
+++ 0114-flatten-binary-tree-to-linked-list/0114-flatten-binary-tree-to-linked-list.cpp
@@ -9,34 +9,24 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
-#include <stack>
-using namespace std;
-
 class Solution {
 public:
+     void preorder(TreeNode* root , vector<TreeNode*> &ans){
+        if(!root)return;
+        ans.push_back(root);
+        preorder(root->left , ans);
+        preorder(root->right , ans);
+     }
     void flatten(TreeNode* root) {
-        if (!root) return;
-
-        stack<TreeNode*> stack;
-        stack.push(root);
-
-        while (!stack.empty()) {
-            TreeNode* current = stack.top();
-            stack.pop();
-
-            if (current->right) {
-                stack.push(current->right);
-            }
-            if (current->left) {
-                stack.push(current->left);
-            }
-
-            if (!stack.empty()) {
-                current->right = stack.top();
-            }
-
-            current->left = nullptr;
+        if(!root)return;
+        vector<TreeNode*> ans;
+        preorder(root , ans);
+        for(int i = 0 ; i < ans.size() - 1 ; i++){
+            ans[i]->left = NULL;
+            ans[i]->right = ans[i+1];
         }
+        ans.back()->left = NULL;
+        ans.back()->right = NULL;
         
     }
 };
