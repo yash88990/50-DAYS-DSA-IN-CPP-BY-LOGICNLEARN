dad89b809c4dd4b9d27309b3468b367111162579
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-28 06:41:18 +0530

    Time: 0 ms (100%), Space: 20.8 MB (88.52%) - LeetHub

--- 0113-path-sum-ii/0113-path-sum-ii.cpp
+++ 0113-path-sum-ii/0113-path-sum-ii.cpp
@@ -4,28 +4,19 @@ public:
     vector<vector<int>> pathSum(TreeNode* root, int targetSum) {
         vector<vector<int>> allPaths;
         vector<int> currentPath;
-        
         dfs(root, targetSum, currentPath, allPaths);
-        
         return allPaths;
     }
-
 private:
     void dfs(TreeNode* node, int remainingSum, vector<int>& currentPath, vector<vector<int>>& allPaths) {
-        if (!node) {
-            return;
-        }
-        
+        if (!node)     return;
         remainingSum -= node->val;
         currentPath.push_back(node->val);
-        
         if (!node->left && !node->right && remainingSum == 0) {
             allPaths.push_back(currentPath);
         }
-        
         dfs(node->left, remainingSum, currentPath, allPaths);
         dfs(node->right, remainingSum, currentPath, allPaths);
-        
         currentPath.pop_back();
     }
 };
