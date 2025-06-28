50a5ef166b8f067b894a12e65c448101df909278
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-28 05:57:46 +0530

    Time: 4 ms (50.47%), Space: 27.4 MB (75.32%) - LeetHub

--- 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
+++ 0106-construct-binary-tree-from-inorder-and-postorder-traversal/0106-construct-binary-tree-from-inorder-and-postorder-traversal.cpp
@@ -1,25 +1,37 @@
-
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
-    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
-        unordered_map<int, int> inorderIndex;
-        for (int i = 0; i < inorder.size(); ++i)inorderIndex[inorder[i]] = i;
-        return buildTreeHelper(inorder, 0, inorder.size() - 1,postorder, 0, postorder.size() - 1,inorderIndex);
-    }
-private:
-    TreeNode* buildTreeHelper(const vector<int>& inorder, int inStart, int inEnd, const vector<int>& postorder, int postStart, int postEnd,
-                              const unordered_map<int, int>& inorderIndex) {
-        if (inStart > inEnd || postStart > postEnd)    return nullptr;
-        // The last element of postorder is the root of the tree.
-        int rootVal = postorder[postEnd];
-        TreeNode* root = new TreeNode(rootVal);
-        // Find the index of the root in inorder traversal.
-        int rootIndex = inorderIndex.at(rootVal);
-        // Calculate the size of the left subtree.
-        int leftSubtreeSize = rootIndex - inStart;
-        // Recursively build the left and right subtrees.
-        root->left = buildTreeHelper(inorder, inStart, rootIndex - 1,postorder, postStart, postStart + leftSubtreeSize - 1,inorderIndex);
-        root->right = buildTreeHelper(inorder, rootIndex + 1, inEnd,postorder, postStart + leftSubtreeSize, postEnd - 1,inorderIndex);
+    TreeNode* solve(vector<int> &inorder , int instart , int inend ,vector<int>&postorder , int poststart  , int  postend , unordered_map<int,int>& inMap){
+        //baese case
+        if(instart > inend || poststart > postend)return NULL;
+        int rootval = postorder[postend];
+        TreeNode* root = new TreeNode(rootval);
+        int rootindex = inMap[rootval];
+        int leftsize = rootindex - instart;
+        root->left = solve(inorder , instart , rootindex - 1 , postorder , poststart , poststart + leftsize  -1  , inMap);
+        root->right = solve(inorder , rootindex + 1 , inend , postorder , poststart + leftsize , postend - 1 , inMap);
         return root;
+
+    }
+
+
+
+
+    TreeNode* buildTree(vector<int>& inorder, vector<int>& postorder) {
+        unordered_map<int,int> inMap;
+        for(int i = 0 ;i < inorder.size() ; i++){
+            inMap[inorder[i]] = i;
+        }
+        return solve(inorder , 0 , inorder.size() - 1 , postorder , 0 , postorder.size() -1  , inMap);
     }
 };
