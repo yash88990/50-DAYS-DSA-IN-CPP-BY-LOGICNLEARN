4f0b8491aba9010bc101447f95740734cf565799
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-28 07:18:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Serialize and deserialize a binary tree/serialize-and-deserialize-a-binary-tree.cpp
+++ Difficulty: Medium/Serialize and deserialize a binary tree/serialize-and-deserialize-a-binary-tree.cpp
@@ -11,40 +11,43 @@ class Solution {
   public:
     // Function to serialize a tree and return a list containing nodes of tree.
     vector<int> serialize(Node *root) {
-        vector<int> result;
-        if (!root) return result;
+        // Your code here
+        vector<int>result;
+        if(!root)return result;
         queue<Node*> q;
         q.push(root);
-        while (!q.empty()) {
+        while(!q.empty()){
             Node* node = q.front();
             q.pop();
-            if (node) {
+            if(node){
                 result.push_back(node->data);
                 q.push(node->left);
                 q.push(node->right);
-            } else  result.push_back(-1);  // Using -1 as a placeholder for NULL
+            }else{
+                result.push_back(-1);
+            }
         }
         return result;
     }
 
     // Function to deserialize a list and construct the tree.
     Node *deSerialize(vector<int> &arr) {
-        if (arr.empty()) return NULL;
+        
+        // Your code here
+        if(arr.empty())return NULL;
         queue<Node*> q;
-        Node *root = new Node(arr[0]);
+        Node* root = new Node(arr[0]);
         q.push(root);
         int i = 1;
-        while (!q.empty() && i < arr.size()) {
+        while(!q.empty() && i < arr.size()){
             Node* parent = q.front();
             q.pop();
-            // Left child
-            if (arr[i] != -1) {
+            if(arr[i] != -1){
                 parent->left = new Node(arr[i]);
                 q.push(parent->left);
             }
             i++;
-            // Right child
-            if (i < arr.size() && arr[i] != -1) {
+            if(i < arr.size() && arr[i] != -1){
                 parent->right = new Node(arr[i]);
                 q.push(parent->right);
             }
@@ -53,5 +56,3 @@ class Solution {
         return root;
     }
 };
-
-
