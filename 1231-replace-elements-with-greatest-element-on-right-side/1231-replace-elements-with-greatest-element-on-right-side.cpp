a8b59a89b565ee54c9a9c928411d6bf32e1f5a20
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-06-30 08:54:03 +0530

    Time: 0 ms (100%), Space: 72.7 MB (73.79%) - LeetHub

--- 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
+++ 1231-replace-elements-with-greatest-element-on-right-side/1231-replace-elements-with-greatest-element-on-right-side.cpp
@@ -2,15 +2,12 @@ class Solution {
 public:
     vector<int> replaceElements(vector<int>& arr) {
         int n = arr.size();
-        int max_so_far = -1; // Last element should be -1
-
-        // Traverse from right to left
-        for (int i = n - 1; i >= 0; --i) {
-            int temp = arr[i];      // Store current value
-            arr[i] = max_so_far;    // Replace with max_so_far
-            max_so_far = max(max_so_far, temp); // Update max
+        int maxi = -1;
+        for(int i = n-1 ; i >= 0 ; i--){
+            int temp = arr[i];
+            arr[i] = maxi;
+            maxi = max(maxi , temp);
         }
-
         return arr;
     }
 };
