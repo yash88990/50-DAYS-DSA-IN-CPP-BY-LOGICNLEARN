5912a55326eab441ad2408c0b1e73fc263fb6c39
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-07-03 14:02:23 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sum Tree/sum-tree.cpp
+++ Difficulty: Medium/Sum Tree/sum-tree.cpp
@@ -8,22 +8,18 @@ struct Node
 // Should return true if tree is Sum Tree, else false
 class Solution {
   public:
-   pair<bool, int> solve(Node* root){
-       if(!root)return {true , 0};
-       if(!root->left && !root->right)return {true , root->data};
-       pair<bool , int> leftans = solve(root->left);
-       pair<bool , int> rightans = solve(root->right);
-       if(!leftans.first || !rightans.first)return {false, 0};
-       int sum = leftans.second + rightans.second;
-       if(root->data == sum){
-           return {true , sum + root->data};
-       }else{
-           return {false, 0};
-       }
-   }
+  pair<bool,int>solve(Node* root){
+      if(!root)return {true, 0};
+      if(!root->left && !root->right)return {true , root->data};
+      pair<bool,int> leftans = solve(root->left);
+      pair<bool,int> rightans = solve(root->right);
+      int sum = leftans.second + rightans.second;
+      if(root->data == sum){
+          return {true , sum + root->data};
+      }else return {false, 0};
+  }
     bool isSumTree(Node* root) {
         // Your code here
-        
         return solve(root).first;
     }
 };
