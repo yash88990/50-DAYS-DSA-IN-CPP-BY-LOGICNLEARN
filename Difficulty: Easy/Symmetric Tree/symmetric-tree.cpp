22ca8db952eeabcdca71525687d1b71f4d193315
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-07-03 14:23:47 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Symmetric Tree/symmetric-tree.cpp
+++ Difficulty: Easy/Symmetric Tree/symmetric-tree.cpp
@@ -12,11 +12,9 @@ public:
 */
 bool solve(Node* root1, Node* root2)
     {
-        if(root1==NULL && root2==NULL)
-        return true;
+        if(!root1 && !root2) return true;
         
-        if((root1==NULL && root2!=NULL) || (root1!=NULL && root2==NULL) || (root1->data != root2->data) )
-        return false;
+        if((!root1  || !root2) || (root1->data != root2->data))return false;
         
         bool check1= solve(root1->left,root2->right);
         bool check2= solve(root1->right,root2->left);
@@ -28,8 +26,7 @@ class Solution {
   public:
     bool isSymmetric(Node* root) {
         // Code here
-        if(root==NULL)
-        return true;
+        if(!root) return true;
         
         return solve(root->left,root->right);
     }
