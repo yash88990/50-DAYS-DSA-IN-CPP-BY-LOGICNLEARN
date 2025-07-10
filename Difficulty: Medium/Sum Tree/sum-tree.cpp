79322bee4164e106efea7e1bee26767138894e27
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-07-10 13:28:32 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Medium/Sum Tree/sum-tree.cpp
+++ Difficulty: Medium/Sum Tree/sum-tree.cpp
@@ -13,10 +13,15 @@ class Solution {
       if(!root->left && !root->right)return {true , root->data};
       pair<bool,int> leftans = solve(root->left);
       pair<bool,int> rightans = solve(root->right);
+      if(!leftans.first || !rightans.first)return {false, 0};
       int sum = leftans.second + rightans.second;
-      if(root->data == sum){
-          return {true , sum + root->data};
-      }else return {false, 0};
+      if(sum == root->data){
+          return {true, sum + root->data};
+      }else{
+          return {false , 0};
+      }
+      
+      
   }
     bool isSumTree(Node* root) {
         // Your code here
