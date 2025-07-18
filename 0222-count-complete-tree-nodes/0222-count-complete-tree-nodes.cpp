2e41d44c6f04eddb4d62c4a0c5c6296d518ec0bb
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-07-18 07:15:36 +0530

    Time: 0 ms (100%), Space: 31.2 MB (94.42%) - LeetHub

--- 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
+++ 0222-count-complete-tree-nodes/0222-count-complete-tree-nodes.cpp
@@ -11,8 +11,16 @@
  */
 class Solution {
 public:
+    void inorder(TreeNode* root  , int &count){
+        if(!root)return;
+        inorder(root->left , count);
+        count++;
+        inorder(root->right , count);
+    }
     int countNodes(TreeNode* root) {
         if(!root)return 0;
-        return 1 + countNodes(root->left) + countNodes(root->right);
+        int count =0;
+        inorder(root , count);
+        return count;
     }
 };
