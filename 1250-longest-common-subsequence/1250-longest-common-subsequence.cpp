e322ed0d8057ac2fc1ab037f053c075f32f9523f
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-08-06 09:58:07 +0530

    Time: 11 ms (98.21%), Space: 9 MB (96.44%) - LeetHub

--- 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
+++ 1250-longest-common-subsequence/1250-longest-common-subsequence.cpp
@@ -23,9 +23,26 @@ public:
         }
         return dp[0][0];
     }
+    int spaceopti(string a , string b){
+        vector<int> curr(b.length() + 1 , 0);
+        vector<int> next(b.length() +1  , 0);
+        for(int i = a.length() - 1 ; i >= 0 ; i--){
+            for(int j = b.length() -1 ; j >= 0 ; j--){
+                int ans = 0;
+                if(a[i] == b[j])ans = 1 + next[j+1];
+                else ans = max(next[j] , curr[j+1]);
+                curr[j] = ans;
+            }
+            next = curr;
+            
+        }
+        return next[0];
+
+    }
     int longestCommonSubsequence(string text1, string text2) {
         // vector<vector<int>>dp(text1.size() , vector<int>(text2.size() , -1));
         // return solve(text1 , text2, 0,0 , dp);
-        return solvetabulation(text1, text2);
+        // return solvetabulation(text1, text2);
+        return spaceopti(text1, text2);
     }
 };
