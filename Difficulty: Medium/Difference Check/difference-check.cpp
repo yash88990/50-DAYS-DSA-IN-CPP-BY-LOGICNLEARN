243f511683eac1f2168f6a52c4da0c96c6177810
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-08-07 13:10:46 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Difference Check/difference-check.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int toSec(string &s){
+        return stoi(s.substr(0,2)) * 3600 + stoi(s.substr(3, 2)) * 60 + stoi(s.substr(6,2));
+    }
+    int minDifference(vector<string> &arr) {
+        // code here
+        vector<bool> vis(86400, false);
+        for(auto &it: arr){
+            int sec = toSec(it);
+            if(vis[sec]) return 0;
+            vis[sec] = true;
+        }
+        int one = -1, two = -1, mini = INT_MAX;
+        for(int i=0; i<86400; i++){
+            if(!vis[i]) continue;
+            if(two != -1) mini = min(mini, i-two);
+            else one = i;
+            two = i;
+        }
+        mini = min(mini, one+86400-two);
+        return mini;
+    }
+};
+
