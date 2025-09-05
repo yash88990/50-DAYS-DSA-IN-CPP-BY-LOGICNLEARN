a7b0bf74d2ce1e9fea719262e598a1d0231a62ac
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-05 15:18:21 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Modular Exponentiation/modular-exponentiation.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int solve(int x, int n, int M) {
+        if (n == 0) return 1;
+        long long temp = solve(x, n / 2, M);
+
+        // Cast to long long for the multiplication to prevent overflow
+        temp = (temp * temp) % M;
+
+        if (n % 2 == 0)
+            return temp;
+        else
+            return ((long long)x * temp) % M;
+    }
+    
+    int powMod(int x, int n, int M) {
+        // code here
+        return solve(x, n, M);
+    }
+};
