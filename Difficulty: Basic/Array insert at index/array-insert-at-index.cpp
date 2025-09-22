21d68c0dad2c53d50f45ebd4e9c46ddcc33e14d2
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-09-22 13:00:25 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Basic/Array insert at index/array-insert-at-index.cpp
+++ Difficulty: Basic/Array insert at index/array-insert-at-index.cpp
@@ -2,7 +2,15 @@ class Solution {
   public:
     void insertAtIndex(vector<int> &arr, int index, int val) {
         // code here
-        arr.insert(arr.begin() + index, val);
+        //method 1 
+        // arr.insert(arr.begin() + index, val);
         
+        //method 2 
+        int n = arr.size();
+        arr.resize(n+1);
+        for(int i = n; i > index ; i--){
+            arr[i] = arr[i-1];
+        }
+        arr[index] = val;
     }
 };
