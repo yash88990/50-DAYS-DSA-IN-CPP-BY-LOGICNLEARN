579a366e06d10a5d3dad4679b8c5e72062bdb73e
Chandan Kumar Chaudhary <125543953+yash88990@users.noreply.github.com>
2025-10-06 21:36:14 +0530

    Updated Solution - GfG to GitHub

--- Difficulty: Easy/Delete Array Elements/delete-array-elements.cpp
+++ Difficulty: Easy/Delete Array Elements/delete-array-elements.cpp
@@ -2,21 +2,19 @@ class Solution {
   public:
     vector<int> deleteElement(vector<int>& arr, int k) {
         // complete the function
-        vector<int> res;
+        vector<int>res;
         int n = arr.size();
-    res.resize(n);
-    int ptr = -1;
-    for (int i = 0; i < n; i++) {
-        while (ptr >-1 &&  res[ptr] < arr[i] && k > 0) {
-            ptr--;
-            k--;
+        res.resize(n);
+        int prev = -1;
+        for(int i = 0; i < n ; i++){
+            while(prev > -1 && res[prev] < arr[i] && k > 0){
+                k--;
+                prev--;
+            }
+            prev++;
+            res[prev] = arr[i];
         }
-        
-        ptr++;
-        res[ptr] = arr[i];
-    }
-    res.resize(ptr + 1);
-    
-    return res;
+        res.resize(prev + 1);
+        return res;
     }
 };
